caf553008664f23d28f6db297cb7a6eb23bed47e
KhalidIbra <90557979+KhalidIbra@users.noreply.github.com>
2022-02-11 11:25:45 +0000

    Add files via upload

---
+++ SudokuSolver.py
@@ -0,0 +1,90 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def solve(brd):
+
+    find = find_blank(brd)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1, 10):
+        if valid(brd, i, (row, col)):
+            brd[row][col] = i
+
+            if solve(brd):
+                return True
+
+            brd[row][col] = 0
+
+
+
+
+
+
+def valid(brd, num, pos):
+
+    for i in range(len(brd[0])):
+        if brd[pos[0]][i] == num and pos[1] != i:
+            return False
+# Checks if there are any duplicates in a row
+
+    for i in range(len(brd)):
+        if brd[i][pos[1]] == num and pos[0] != i:
+            return False
+# Checks duplicates in column
+
+
+    box_a = pos[1] // 3
+    box_b = pos[0] // 3
+
+    for i in range(box_b*3, box_b*3, +3):
+        for j in range(box_a*3, box_a*3, +3):
+            if brd[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+
+# Sudoku Board Visualisation
+# i = row , j = column
+def print_board(brd):
+    for i in range(len(brd)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - ")
+
+        for j in range(len(brd[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(brd[i][j])
+            else:
+              print(str(brd[i][j]) + " ", end="")
+
+
+
+
+def find_blank(brd):
+    for i in range(len(brd)):
+        for j in range(len(brd[0])):
+            if brd[i][j] == 0:
+                return (i , j)
+
+
+
+
+print_board(board)
+solve(board)
+print("__________________")
+print_board(board)
